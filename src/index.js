55d8d9dcb0d75d713afbc98225a84a9852bd211b
Fuka <mizunofuka0102@gmail.com>
2023-01-30 14:21:59 +0000

    追加クリックでTODOを追加する処理を実装

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import "./styles.css";
+
+const onCLickAdd = () => {
+  // テキストボックスの値を取得し初期化する
+  const inputText = document.querySelector("#add-text").value;
+  document.querySelector("#add-text").value = "";
+
+  const li = document.createElement("li");
+  li.className = "list-row";
+  li.innerText = inputText;
+  document.querySelector(".imcomplete-area-list").appendChild(li);
+};
+
+document
+  .querySelector("#add-button")
+  .addEventListener("click", () => onCLickAdd());
