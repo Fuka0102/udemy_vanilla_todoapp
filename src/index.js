9eb43e2d6570ee19bb207675deeb1f007883c150
Fuka <mizunofuka0102@gmail.com>
2023-01-31 13:48:16 +0000

    タスクの追加機能を実装

--- src/index.js
+++ src/index.js
@@ -9,6 +9,25 @@ const onCLickAdd = () => {
   li.className = "list-row";
   li.innerText = inputText;
   document.querySelector(".imcomplete-area-list").appendChild(li);
+
+  const div = document.createElement("div");
+  div.className = "button-area";
+
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    alert("完了");
+  });
+
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    alert("削除");
+  });
+
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
+  li.appendChild(div);
 };
 
 document
